830e09fe29d3558c85d324b2c06e6cc09560e26a
KASSIME <giovana.kassime.tds2023@gmail.com>
2025-01-31 15:36:39 -0300

    aaaaaaaa

---
+++ app.js
@@ -0,0 +1,8 @@
+function carregaArquivo(container, arquivo){
+    fetch(arquivo).then(arq => arq.text()).then(elemento => {
+        document.getElementById(container).innerHTML = elemento
+    }).catch(erro => console.log("erro ao carregar" + arquivo))
+}
+
+carregaArquivo('container-sidebar', 'sidebar.html')
+carregaArquivo('container-navbar', 'navbar.html')
